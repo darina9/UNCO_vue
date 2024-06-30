77afd7b3e5349119e3cf48bf1be64a40d5f3bfe1
darina9 <111124490+darina9@users.noreply.github.com>
2024-06-30 17:54:44 +0200

    Add files via upload

---
+++ src/view/MainPage.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="MainPage">
+    
+
+    <HeaderComponent/>
+    <main class="center">
+    <BannerInfo />
+    <BlockchainContracts/>
+    <BannerImg/>
+    <ProfitChart/>
+    <ProfitList/>
+    <BannerWatch/>
+    <BannerFounder/>
+    <ChartsDifference/>
+    <WorkflowAlgorithm/>
+    <BannerFarmer/>
+    <FaqList/>
+    <OtherServices/>
+    </main>
+    <FooterComponent/>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from '../components/HeaderComponent.vue'
+import BannerInfo from '../components/BannerInfo.vue'
+import BlockchainContracts from '../components/BlockchainContracts.vue'
+import BannerImg from '../components/BannerImg.vue'
+import ProfitChart from '../components/ProfitChart.vue'
+import ProfitList from '../components/ProfitList.vue'
+import BannerWatch from '../components/BannerWatch.vue';
+import BannerFounder from '../components/BannerFounder.vue'
+import ChartsDifference from '../components/ChartsDifference.vue'
+import WorkflowAlgorithm from '@/components/WorkflowAlgorithm.vue'
+import BannerFarmer from '@/components/BannerFarmer.vue'
+
+import FaqList from '../components/FaqList.vue'
+import OtherServices from '../components/OtherServices.vue'
+import FooterComponent from '../components/FooterComponent.vue'
+
+export default {
+  name: 'MainPage',
+  components: {
+    HeaderComponent,
+    BannerInfo,
+    BlockchainContracts,
+    BannerImg,
+    ProfitChart,
+    ProfitList,
+    BannerWatch,
+    BannerFounder,
+    ChartsDifference,
+    WorkflowAlgorithm,
+    BannerFarmer,
+    FaqList,
+    OtherServices,
+    FooterComponent
+  }
+}
+</script>
+
+<style >@import url("../assets/common-styles.css");
+</style>
