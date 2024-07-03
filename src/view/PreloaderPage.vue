735799d8e693ff5472843d8c64104fcce6f16fcb
darina9 <111124490+darina9@users.noreply.github.com>
2024-07-03 20:22:46 +0200

    Add files via upload

---
+++ src/view/PreloaderPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <PreloaderComponent />
+  </template>
+  
+  <script>
+  import PreloaderComponent from '@/components/PreloaderComponent.vue';
+  
+  export default {
+    name: 'PreloaderPage',
+    components: {
+      PreloaderComponent,
+    },
+  };
+  </script>
