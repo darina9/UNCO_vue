32971b228be30ebd1f101c84429425eefa1b3548
darina9 <111124490+darina9@users.noreply.github.com>
2024-07-03 20:23:17 +0200

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,16 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import MainPage from './view/MainPage.vue';
+import MainPage from '@/view/MainPage.vue';
+import PreloaderPage from '@/view/PreloaderPage.vue';
 
 const routes = [
   {
     path: '/',
+    name: 'PreloaderPage',
+    component: PreloaderPage,
+  },
+  {
+    path: '/main',
     name: 'MainPage',
     component: MainPage,
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
