b34bcf2b721b531331ca37d6c847ef17092101c5
darina9 <111124490+darina9@users.noreply.github.com>
2024-07-17 20:05:49 +0200

    Update ChartsDifference.vue

--- src/components/ChartsDifference.vue
+++ src/components/ChartsDifference.vue
@@ -1,65 +1,86 @@
 <template>
-    <section class="charts-difference">
-      <h5 class="charts-difference__title">{{ $t('charts_difference.title') }}</h5>
-      <div class="charts-difference__imgs">
+  <section class="charts-difference">
+    <h5 class="charts-difference__title">
+      {{ $t("charts_difference.title") }}
+    </h5>
+    <div class="charts-difference__imgs">
+      <div class="charts-difference__imgs_left">
+        <img
+          class="charts-difference__imgs_img"
+          src="../assets/img/image_potatoes.png"
+          alt="Potatoes"
+        />
+        <h6 class="charts-difference__imgs_title">
+          {{ $t("charts_difference.potatoes_title") }}
+        </h6>
+        <img
+          class="charts-difference__imgs_chart"
+          :src="potatoesChartSrc"
+          alt="chart potatoes"
+        />
+      </div>
+      <div class="chart-difference__imgs_right">
         <div class="charts-difference__imgs_left">
-          <img class="charts-difference__imgs_img" src="../assets/img/image_potatoes.png" alt="Potatoes" />
-          <h6 class="charts-difference__imgs_title">{{ $t('charts_difference.potatoes_title') }}</h6>
-          <img class="charts-difference__imgs_chart" :src="potatoesChartSrc" alt="chart potatoes" />
-        </div>
-        <div class="chart-difference__imgs_right">
-          <div class="charts-difference__imgs_left">
-            <img class="charts-difference__imgs_img" src="../assets/img/image_peaches.png" alt="peaches" />
-            <h6 class="charts-difference__imgs_title">{{ $t('charts_difference.peaches_title') }}</h6>
-            <img class="charts-difference__imgs_chart" :src="peachesChartSrc" alt="chart peaches" />
-          </div>
+          <img
+            class="charts-difference__imgs_img"
+            src="../assets/img/image_peaches.png"
+            alt="peaches"
+          />
+          <h6 class="charts-difference__imgs_title">
+            {{ $t("charts_difference.peaches_title") }}
+          </h6>
+          <img
+            class="charts-difference__imgs_chart"
+            :src="peachesChartSrc"
+            alt="chart peaches"
+          />
         </div>
       </div>
-      <div class="chart-difference__btn">
-       <button class="btn btn-big" @click="scrollToFooter">
-      {{ $t('charts_difference.button') }}
-    </button>
-      </div>
-    </section>
-  </template>
+    </div>
+    <div class="chart-difference__btn">
+      <button class="btn btn-big" @click="scrollToFooter">
+        {{ $t("charts_difference.button") }}
+      </button>
+    </div>
+  </section>
+</template>
   
   <script>
-  import { useI18n } from 'vue-i18n';
-  import { computed } from 'vue';
-  
-  export default {
-    name: 'ChartsDifference',
-    setup() {
-      const { t, locale } = useI18n();
-  
-      const getChartSrc = (baseName) => {
-        if (locale.value === 'ru') {
-          return require(`../assets/img/${baseName}_ru.png`);
-        } else if (locale.value === 'de') {
-          return require(`../assets/img/${baseName}_de.png`);
-        } 
-        else if (locale.value === 'uk') {
-          return require(`../assets/img/${baseName}_uk.png`);
-        }else {
-          return require(`../assets/img/${baseName}.png`);
-        }
-      };
-  
-      const potatoesChartSrc = computed(() => getChartSrc('chart_potatoes'));
-      const peachesChartSrc = computed(() => getChartSrc('chart_peaches'));
-      const scrollToFooter = () => {
-      const footer = document.querySelector('footer');
-      if (footer) {
-        footer.scrollIntoView({ behavior: 'smooth' });
+import { useI18n } from "vue-i18n";
+import { computed } from "vue";
+
+export default {
+  name: "ChartsDifference",
+  setup() {
+    const { t, locale } = useI18n();
+
+    const getChartSrc = (baseName) => {
+      if (locale.value === "ru") {
+        return require(`../assets/img/${baseName}_ru.png`);
+      } else if (locale.value === "de") {
+        return require(`../assets/img/${baseName}_de.png`);
+      } else if (locale.value === "uk") {
+        return require(`../assets/img/${baseName}_uk.png`);
+      } else {
+        return require(`../assets/img/${baseName}.png`);
       }
     };
-      return { t, potatoesChartSrc, peachesChartSrc, scrollToFooter };
-    },
-  };
-  </script>
+
+    const potatoesChartSrc = computed(() => getChartSrc("chart_potatoes"));
+    const peachesChartSrc = computed(() => getChartSrc("chart_peaches"));
+    const scrollToFooter = () => {
+      const footer = document.querySelector("footer");
+      if (footer) {
+        footer.scrollIntoView({ behavior: "smooth" });
+      }
+    };
+    return { t, potatoesChartSrc, peachesChartSrc, scrollToFooter };
+  },
+};
+</script>
   <style>
-  @import url("../assets/common-styles.css");
-  .charts-difference {
+@import url("../assets/common-styles.css");
+.charts-difference {
   margin-top: 100px;
 }
 .charts-difference__title {
@@ -97,7 +118,7 @@
   width: 708px;
 }
 @media only screen and (max-width: 1439px) and (min-width: 768px) {
-    .charts-difference__title {
+  .charts-difference__title {
     font-size: 24px;
     width: 100%;
   }
@@ -130,7 +151,7 @@
   }
 }
 @media only screen and (max-width: 767px) {
-    .charts-difference {
+  .charts-difference {
     margin-top: 60px;
   }
   .charts-difference__title {
@@ -177,8 +198,5 @@
     text-align: center;
     line-height: 1.6;
   }
-
 }
-
-
-  </style>
+</style>
