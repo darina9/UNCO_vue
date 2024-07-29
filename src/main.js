d47797dc867c976003dd7a00bd9320d99cb17180
darina9 <111124490+darina9@users.noreply.github.com>
2024-07-29 15:26:05 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,22 +6,42 @@ import router from './router';
 import en from './locales/en.json';
 import ru from './locales/ru.json';
 import de from './locales/de.json';
-import uk from './locales/uk.json'; // Добавляем украинский язык
+import uk from './locales/uk.json';
 
 const messages = {
   en,
   ru,
   de,
-  uk // Добавляем украинский язык
+  uk
 };
 
 const i18n = createI18n({
   legacy: false,
-  locale: 'en',
+  locale: 'en', // Установите язык по умолчанию
   messages,
 });
 
 const app = createApp(App);
 app.use(router);
 app.use(i18n);
-app.mount('#app');
+
+router.isReady().then(() => {
+  const lang = router.currentRoute.value.params.lang || getCookie('language') || 'en';
+  i18n.global.locale.value = lang;
+
+  if (!router.currentRoute.value.params.lang) {
+    router.replace({ name: 'MainPage', params: { lang }, query: router.currentRoute.value.query });
+  }
+  app.mount('#app');
+});
+
+function getCookie(name) {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
