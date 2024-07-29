8d14aeb9f8de5c6b3efe40da3ccf6a2b3c37bde7
darina9 <111124490+darina9@users.noreply.github.com>
2024-07-29 15:24:03 +0200

    Update NotFound.vue

---
+++ src/view/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="not-found">
+    <h1>404 - Page Not Found</h1>
+    <p>Sorry, the page you are looking for does not exist.</p>
+    <router-link
+      :to="{ name: 'MainPage', params: { lang: $route.params.lang || 'en' } }"
+      >Go to Homepage</router-link
+    >
+  </div>
+</template>
+  
+  <script>
+export default {
+  name: "NotFound",
+};
+</script>
+  
+  <style>
+.not-found {
+  text-align: center;
+  padding: 50px;
+}
+</style>
+  
