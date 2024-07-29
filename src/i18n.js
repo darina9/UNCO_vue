e212319a68df6959f02f65bf75dd54ce7d30ba1e
darina9 <111124490+darina9@users.noreply.github.com>
2024-07-29 15:25:41 +0200

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -12,6 +12,7 @@ const messages = {
 };
 
 const i18n = createI18n({
+  legacy: false,
   locale: 'en', // set locale
   fallbackLocale: 'en', // set fallback locale
   messages, // set locale messages
