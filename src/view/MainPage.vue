baa45ecd1f075d8b6848fcfcfef22865ef0f8467
darina9 <111124490+darina9@users.noreply.github.com>
2024-07-29 15:23:43 +0200

    Update MainPage.vue

--- src/view/MainPage.vue
+++ src/view/MainPage.vue
@@ -1,44 +1,54 @@
 <template>
-   <div :class="['main-page', { 'fade-in': fadeIn }]">
-    <HeaderComponent/>
-    <main class="center">
-    <BannerInfo />
-    <BlockchainContracts/>
-    <BannerImg/>
-    <ProfitChart/>
-    <ProfitList/>
-    <BannerWatch/>
-    <BannerFounder/>
-    <ChartsDifference/>
-    <WorkflowAlgorithm/>
-    <BannerFarmer/>
-    <FaqList/>
-    <OtherServices/>
-    </main>
-    <FooterComponent/>
+  <div>
+    <PreloaderComponent
+      v-if="showPreloader"
+      :lang="lang"
+      @loaded="handlePreloaderLoaded"
+    />
+    <div v-else class="main-content" :class="{ 'fade-in': !showPreloader }">
+      <HeaderComponent />
+      <main class="center">
+        <BannerInfo />
+        <BlockchainContracts />
+        <BannerImg />
+        <ProfitChart />
+        <ProfitList />
+        <BannerWatch />
+        <BannerFounder />
+        <ChartsDifference />
+        <WorkflowAlgorithm />
+        <BannerFarmer />
+        <FaqList />
+        <OtherServices />
+      </main>
+      <FooterComponent />
+    </div>
   </div>
 </template>
 
 <script>
-import HeaderComponent from '../components/HeaderComponent.vue'
-import BannerInfo from '../components/BannerInfo.vue'
-import BlockchainContracts from '../components/BlockchainContracts.vue'
-import BannerImg from '../components/BannerImg.vue'
-import ProfitChart from '../components/ProfitChart.vue'
-import ProfitList from '../components/ProfitList.vue'
-import BannerWatch from '../components/BannerWatch.vue';
-import BannerFounder from '../components/BannerFounder.vue'
-import ChartsDifference from '../components/ChartsDifference.vue'
-import WorkflowAlgorithm from '@/components/WorkflowAlgorithm.vue'
-import BannerFarmer from '@/components/BannerFarmer.vue'
-
-import FaqList from '../components/FaqList.vue'
-import OtherServices from '../components/OtherServices.vue'
-import FooterComponent from '../components/FooterComponent.vue'
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+import PreloaderComponent from "@/components/PreloaderComponent.vue";
+import HeaderComponent from "@/components/HeaderComponent.vue";
+import BannerInfo from "@/components/BannerInfo.vue";
+import BlockchainContracts from "@/components/BlockchainContracts.vue";
+import BannerImg from "@/components/BannerImg.vue";
+import ProfitChart from "@/components/ProfitChart.vue";
+import ProfitList from "@/components/ProfitList.vue";
+import BannerWatch from "@/components/BannerWatch.vue";
+import BannerFounder from "@/components/BannerFounder.vue";
+import ChartsDifference from "@/components/ChartsDifference.vue";
+import WorkflowAlgorithm from "@/components/WorkflowAlgorithm.vue";
+import BannerFarmer from "@/components/BannerFarmer.vue";
+import FaqList from "@/components/FaqList.vue";
+import OtherServices from "@/components/OtherServices.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
 
 export default {
-  name: 'MainPage',
+  name: "MainPage",
   components: {
+    PreloaderComponent,
     HeaderComponent,
     BannerInfo,
     BlockchainContracts,
@@ -52,31 +62,45 @@ export default {
     BannerFarmer,
     FaqList,
     OtherServices,
-    FooterComponent
+    FooterComponent,
   },
-  data() {
+  setup() {
+    const route = useRoute();
+    const showPreloader = ref(true); // Переменная для отображения прелоадера
+    const lang = ref(route.params.lang); // Язык из параметров маршрута
+
+    const handlePreloaderLoaded = () => {
+      showPreloader.value = false; // Скрываем прелоадер после завершения
+      console.log("Handling preloader loaded");
+    };
+
+    onMounted(() => {
+      console.log("MainPage mounted with language:", lang.value);
+    });
+
     return {
-      fadeIn: false
+      showPreloader,
+      lang,
+      handlePreloaderLoaded,
     };
   },
-  mounted() {
-    setTimeout(() => {
-      this.fadeIn = true;
-    }, 200); // Delay for fade-in effect
-  }
 };
-
 </script>
 
-<style >@import url("../assets/common-styles.css");
-.main-page {
+<style>
+@import url("../assets/common-styles.css");
+
+
+.main-content {
   opacity: 0;
   transition: opacity 0.5s ease-in;
 }
 
-.main-page.fade-in {
+.main-content.fade-in {
   opacity: 1;
 }
 </style>
+
+
 
 
