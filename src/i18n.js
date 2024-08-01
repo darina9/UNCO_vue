04984b9b4b48336a6fcf59a194b6d2a139afa88c
darina9 <111124490+darina9@users.noreply.github.com>
2024-08-01 20:46:10 +0200

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -2,20 +2,26 @@ import { createI18n } from 'vue-i18n';
 import en from './locales/en.json';
 import ru from './locales/ru.json';
 import de from './locales/de.json';
-import uk from './locales/uk.json'; // Добавляем украинский язык
+import uk from './locales/uk.json';
+import fr from './locales/fr.json'; // Добавляем французский язык
+import es from './locales/es.json'; // Добавляем испанский язык
+import it from './locales/it.json'; // Добавляем итальянский язык
 
 const messages = {
   en,
   ru,
   de,
-  uk // Добавляем украинский язык
+  uk,
+  fr, // Добавляем французский язык
+  es, // Добавляем испанский язык
+  it  // Добавляем итальянский язык
 };
 
 const i18n = createI18n({
   legacy: false,
-  locale: 'en', // set locale
-  fallbackLocale: 'en', // set fallback locale
-  messages, // set locale messages
+  locale: 'en', // Устанавливаем язык по умолчанию
+  fallbackLocale: 'en', // Устанавливаем запасной язык
+  messages, // Устанавливаем сообщения на разных языках
 });
 
 export default i18n;
